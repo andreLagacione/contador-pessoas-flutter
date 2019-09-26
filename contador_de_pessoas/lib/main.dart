6f4955b5e40e190ea37877ade605bb5445a774e5
andreLagacione <lagacioneandre@gmail.com>
2019-09-25 21:02:30 -0300

    first commit

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -4,7 +4,26 @@ void main() {
   runApp(
     MaterialApp(
       title: "Contador de Pessoas",
-      home: Container(color: Colors.white)
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoa: 0",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+            )
+          ),
+          Text(
+              "Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30
+              )
+          )
+        ],
+      )
     )
   );
 }
